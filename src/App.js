04bc6c80dbc537d10ed4c1a25f062f207dd47323
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-08-15 00:08:21 -0300

    configure drag in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import GlobalStyles from "../src/styles/global";
 import Header from "./components/Header";
 import Board from "./components/Board";
 
+import {DndProvider  } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       <GlobalStyles />
-    </>
+    </DndProvider>
   );
 }
 
